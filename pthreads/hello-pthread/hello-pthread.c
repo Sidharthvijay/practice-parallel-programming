b027d75a6159bc519dd345e272844d451684b959
Pradeep Singh <pdeepsingh91@gmail.com>
2018-02-26 20:20:45 -0800

    hello world with pthread

---
+++ pthreads/hello-pthread/hello-pthread.c
@@ -0,0 +1,35 @@
+/*
+ * File : hello-pthread.c
+ * Author : Pradeep Singh 
+ * Date : 2/26/2018
+ * Description: First pthread program; prints "hello world!" for every thread created.
+ */ 
+
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define NUM_THREADS	5
+
+void *PrintHello(void *threadid)
+{
+   long tid;
+   tid = (long)threadid;
+   printf("Hello World! Thread number %ld!\n", tid);
+   pthread_exit(NULL);
+}
+
+int main(int argc, char *argv[])
+{
+   pthread_t threads[NUM_THREADS];
+   int rc;
+   long t;
+   for(t=0;t<NUM_THREADS;t++){
+      rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
+      if (rc){
+         printf("ERROR; return code from pthread_create() is %d\n", rc);
+         exit(-1);
+      } 
+   }
+   pthread_exit(NULL);
+}
+
