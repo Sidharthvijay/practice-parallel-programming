85d9135b40e2bfc9a61a662a4b8c06622030c4ae
Pradeep Singh <pdeepsingh91@gmail.com>
2018-03-01 12:28:07 -0800

    commit

--- pthreads/hello-pthread/hello-pthread.c
+++ pthreads/hello-pthread/hello-pthread.c
@@ -3,9 +3,6 @@
  * Author : Pradeep Singh 
  * Date : 2/26/2018
  * Description: First pthread program; prints "hello world!" for every thread created.
- * Program usage: 
- *                gcc -o hello-pthread hello-pthread.c -lpthread    // compile using -lpthread (library for pthread)
- *                ./hello-pthread                                   // run
  */ 
 
 #include <pthread.h>
@@ -24,10 +21,10 @@ void *PrintHello(void *threadid)
 int main(int argc, char *argv[])
 {
    pthread_t threads[NUM_THREADS];
-   int a;
+   int rc;
    long t;
    for(t=0;t<NUM_THREADS;t++){
-      a = pthread_create(&threads[t], NULL, PrintHello, (void *)t);
+      rc = pthread_create(&threads[t], NULL, PrintHello, (void *)t); 
    }
    pthread_exit(NULL);
 }
