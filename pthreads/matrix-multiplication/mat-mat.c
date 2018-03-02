9436f2203c2c637a57715d5bf8d691a25e943a72
Pradeep Singh <pdeepsingh91@gmail.com>
2018-03-02 09:46:32 -0800

    Update mat-mat.c

--- pthreads/matrix-multiplication/mat-mat.c
+++ pthreads/matrix-multiplication/mat-mat.c
@@ -49,3 +49,86 @@ void* multiplication(void* arg)
     for (i = row_start; i < row_end; ++i) {
         for (j = 0; j < SIZE; ++j) {
             for (k = 0; k < SIZE; ++k) {
+                 result[i][j] += mat1[i][k] * mat2[k][j];
+            }
+        }
+    }
+    return 0;
+}
+
+/* Main program starts here */
+int main(int argc, char *argv[])
+{
+    int a = 1.0;
+    int i, j;
+
+    struct timeval start, end;
+    gettimeofday(&start, NULL);                         /* start measuring time */
+
+    if (argc != 2) {
+       fprintf( stderr, "%s <number of threads>\n", argv[0] );
+       return -1;
+    }
+
+    NUM_THREADS = atoi( argv[1] );
+
+    /* Dynamic memory allocation for Matrices*/
+    /* Allocate memory for matrix rows and columns = 1000 X 1000 */
+
+    /* Allocating memory to rows of all 3 matrices */
+    mat1 = (double **) malloc(SIZE * sizeof(double *));
+    mat2 = (double **) malloc(SIZE * sizeof(double *));
+    result = (double **) malloc(SIZE * sizeof(double *));
+
+    /* Allocating memory to cols of all 3 matrices */
+    for (i=0;i<SIZE;i++){
+        mat1[i] = (double *) malloc(SIZE * sizeof(double));
+        mat2[i] = (double *) malloc(SIZE * sizeof(double));
+        result[i] = (double *) malloc(SIZE * sizeof(double));
+    }
+
+    /* Generating matrix elements with random numbers between 0 and 1 */
+    srand(time(NULL));
+    for (i = 0; i < SIZE; i++) {
+        for (j = 0; j < SIZE; j++) {
+            mat1 [i][j] = (double) rand()/ (double) (RAND_MAX/a);
+            mat2 [i][j] = (double) rand()/ (double) (RAND_MAX/a);
+        }
+    }
+
+    /* Declare the threads */
+    pthread_t threads[NUM_THREADS];
+    
+   int *param  = (int *)malloc(sizeof(int));
+   for (i = 0; i < NUM_THREADS; ++i)
+       pthread_create(&threads[i], NULL, multiplication, (void *)param);
+   }
+
+   /* Join threads */
+   for (i = 0; i < NUM_THREADS; ++i){
+       pthread_join(threads[i], NULL);
+   }
+
+   /* Free the allocated memory from matrices */
+   for (i = 0; i < SIZE; i++){
+       free(mat1[i]);
+       free(mat2[i]);
+       free(result[i]);
+   }
+
+   free(mat1);
+   free(mat2);
+   free(result);
+   free(param);
+
+   mat1 = NULL;
+   mat2 = NULL;
+   result = NULL;
+
+   gettimeofday(&end, NULL);    /* function to measure execution time */
+
+   /* Print the time elapsed */
+   printf("\n Time Elapsed: %fs \n", ((end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec)/1000000.0));
+
+   return 0;
+}
