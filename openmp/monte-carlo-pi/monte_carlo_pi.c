23b34172dfc2a487664cbe35f63fc8deb09f2b60
Pradeep Singh <pdeepsingh91@gmail.com>
2018-03-18 12:48:07 -0700

    Update monte_carlo_pi.c

--- openmp/monte-carlo-pi/monte_carlo_pi.c
+++ openmp/monte-carlo-pi/monte_carlo_pi.c
@@ -1,49 +1,95 @@
 /*
  * Filename: pi.c
  * Author: Pradeep Singh
- * Date: 3/12/2018
- * Description: OpenMP implementation of Monte Carlo pi-finding algorithm
+ * Date: 03/12/18
+ * Desctiption: The program pi.c is used to implement the evaluation of pi using openmp.
  *
- * Usage:    gcc -Wall -Werror -fopenmp pi.c -o pi   // compile
- *           ./pi <threads>                          // run -- with # of threads
+ *             o Program usage is as,
+ *
+ *                          - gcc -fopenmp pi.c -o pi                                  // for compiling the program
+ *                          - time ./pi   (thread count)  (number of iterations)        // run with time function
+ *                          - valgrind --tool=memcheck --leak-check=yes ./pi           // check for segmentation faults (memory leaks)
+ *                          - gcc -Wall -Werror -fopenmp -o pi pi.c                   // it will check if any warnings
+ *
+ *             o  PBS Usage: - qsub -v T=2 batch.pi                                // submit job
+ *                           - qstat                                               // check job status
+ *                           - cat pi.ojob_id                                      // check o/p
  */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include <omp.h>
 #include <sys/time.h>
 #include <time.h>
 
-int main (int argc, char *argv[])
+void Usage(char* prog_name);
+
+static long MULTIPLIER = 1366;
+static long ADDEND = 150889;
+static long PMOD = 714025;
+long random_last = 0;
+
+#pragma omp threadprivate(random_last)
+double randomU()
 {
-  int i, count, samples, nthreads, seed;
-  struct drand48_data drand_buf;
-  double x, y;
-  struct  timeval start, end;
-  samples = 32000000;
-  nthreads = atoi(argv[1]);
-  omp_set_num_threads (nthreads);
-
-  gettimeofday(&start, NULL);
-  count = 0;
-
-#pragma omp parallel private(i, x, y, seed, drand_buf) shared(samples)
-{
-  seed = 1202107158 + omp_get_thread_num() * 1999;
-  srand48_r (seed, &drand_buf);
-
-  #pragma omp for reduction(+:count)
-  for (i=0; i<samples; i++) {
-      drand48_r (&drand_buf, &x);
-      drand48_r (&drand_buf, &y);
-      if (x*x + y*y <= 1.0) count++;
-  }
+   long random_next;
+   random_next = (MULTIPLIER*random_last + ADDEND)%PMOD;
+   random_last = random_next;
+   return ((double)random_next/(double)PMOD);
 }
 
-  gettimeofday(&end, NULL);
 
-  printf("Estimate of pi: %7.5f\n", 4.0*count/samples);
-  printf("\n Time Elapsed: %fs \n", ((end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec)/1000000.0));
+int main(int argc, char* argv[]) {
+   long long n, i;
+   double x=0.0,y=0.0;                     //x,y value for the random coordinate
+   int thread_count;
+   long long count=0;                     //Count holds all the number of how many good coordinates
+   double z=0.0;                          //Used to check if x^2+y^2<=1
+   double pi=0.0;                        //holds approx value of pi
 
-  return 0;
+   if (argc != 3) Usage(argv[0]);
+   thread_count = strtol(argv[1], NULL, 10);
+   n = strtoll(argv[2], NULL, 10);
+   if (thread_count < 1 || n < 1) Usage(argv[0]);
+
+   struct timeval start, end;
+
+   gettimeofday(&start, NULL);
+
+# pragma omp parallel for num_threads(thread_count) \
+      reduction(+: count) private(x,y,z,i)
+
+  for (i=0; i<n; ++i) {
+       x = (double)randomU();      //gets a random x coordinate
+       y = (double)randomU();      //gets a random y coordinate
+       z = ((x*x)+(y*y));          //Checks to see if number is inside unit circle
+       if (z<=1) {
+           ++count;            //if it is, consider it a valid random point
+       }
+   }
+
+   gettimeofday(&end, NULL);    /* function to measure execution time */
+
+    /* Print the execution time */
+    printf("\n Execution Time: %fs \n", ((end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec)/1000000.0));
+
+   pi = ((double)count/(double)(n))*4.0;
+
+
+   printf("With n = %lld terms and %d threads,\n", n, thread_count);
+   printf("   Our estimate of pi = %.14f\n", pi);
+   printf("                   pi = %.14f\n", 4.0*atan(1.0));
+   return 0;
+}  /* main */
+
+void Usage(char* prog_name) {
+   fprintf(stderr, "usage: %s <thread_count> <n>\n", prog_name);  /* Change */
+   fprintf(stderr, "   thread_count is the number of threads >= 1\n");  /* Change */
+   fprintf(stderr, "   n is the number of terms and should be >= 1\n");
+   exit(0);
 }
+
+
+
+
